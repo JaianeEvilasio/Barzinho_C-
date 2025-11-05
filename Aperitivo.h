d365f80d8f77198a6be17f3c505bd2c7b7004aed
cloobcfh <claferreirahonorio@gmail.com>
2025-11-04 23:33:08 -0300

    update aperitivo.h

--- Aperitivo.h
+++ Aperitivo.h
@@ -1,17 +1,18 @@
 #ifndef APERITIVO_H
 #define APERITIVO_H
 
+//Classe filha de Produto, voltada para os aperitivos.
 #include "Produto.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Aperitivo : public Produto { // aperitivo é uma subclasse de produto (herança)
+class Aperitivo : public Produto { // Aperitivo é uma subclasse de produto (herança).
 public:
     Aperitivo(const string& n, double p) : Produto(n, p, "Aperitivo") {} 
 
-    void mostrar() const override { // usei esse override para fazer polimorfismo, essa versao substitui o virtual la em produto.h
-        cout << nome << " (R$" << preco << ")" << endl; // isso é oq aparece quando a função mostrar() é chamada e por um ponteiro ou referencia q aponta para aperitivo
+    void mostrar() const override { // O override foi utilizado para fazer polimorfismo, essa versao substitui o virtual la em produto.h.
+        cout << nome << " (R$" << preco << ")" << endl; // Isso é o que aparece quando a função mostrar() é chamada  por um ponteiro ou referencia q aponta para aperitivo.
     }
 };
 
